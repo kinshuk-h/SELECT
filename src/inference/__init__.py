29f14e5ba714ea3551efe3ffc4916d2ccc3bbef7
Kinshuk Vasisht <kinshukv@iisc.ac.in>
2025-01-10 03:41:36 +0530

    Update `inference`: migrate managers and related utils to submodule

--- src/inference/__init__.py
+++ src/inference/__init__.py
@@ -1,6 +1,6 @@
-import warnings
 import importlib
 
+from .utils import *
 from .base import ModelInference
 
 def __test_and_add_backend(inf_impl, mod, mod_depend):
@@ -14,16 +14,53 @@ def __test_and_add_backend(inf_impl, mod, mod_depend):
             ) from exc
 
     curr_mod_ref = importlib.import_module(__name__)
-    setattr(curr_mod_ref, inf_impl, getattr(importlib.import_module(mod, __name__), inf_impl))
+    inf_class = getattr(importlib.import_module(mod, __name__), inf_impl)
+    setattr(curr_mod_ref, inf_impl, inf_class)
+
+    return inf_class
 
 def load_backend(backend):
     if backend == "openai":
-        __test_and_add_backend("OpenAIModelInference"     , ".openai"     , ("openai", "tiktoken"))
+        return __test_and_add_backend("OpenAIModelInference"     , ".openai"     , ("openai", "tiktoken"))
     elif backend == "vllm":
-        __test_and_add_backend("VLLMModelInference"       , ".vllm"       , ("vllm", ))
+        return __test_and_add_backend("VLLMModelInference"       , ".vllm"       , ("vllm", ))
     elif backend == "huggingface":
-        __test_and_add_backend("HuggingFaceModelInference", ".huggingface", ("transformers", "accelerate"))
+        return __test_and_add_backend("HuggingFaceModelInference", ".huggingface", ("transformers", "accelerate"))
+
+def prepare_model_for_inference(model_name: str, backend='huggingface', seed=42):
+    """ Wrapper function to construct a ModelInference object from a model key name """
+
+    model_kwargs = get_inference_params(model_name)
+
+    if model_name.lower().startswith("openai"):
+        model_name = model_name[7:]
+
+    return load_backend(backend)(model_name, seed=seed, **model_kwargs)
+
+class ModelManager:
+    """ Context Manager for model inference: Maintains resources for a model until a specified context. """
+
+    def __init__(self, *args, model_initializer=prepare_model_for_inference, **kwargs) -> None:
+        self.model_initializer = model_initializer
+        self.args = args
+        self.kwargs = kwargs
+
+    def __enter__(self) -> ModelInference:
+        sync_vram()
+
+        self.model = self.model_initializer(*self.args, **self.kwargs)
+        return self.model
+
+    def __exit__(self, exc_type, exc_val, exc_traceback):
+        if hasattr(self, 'model') and self.model is not None:
+            if hasattr(self.model, 'unload') and callable(self.model.unload):
+                self.model.unload()
+            del self.model
+        sync_vram()
+
+        if exc_val is not None: raise exc_val
 
 __all__ = [
     "ModelInference",
+    "ModelManager"
 ]
--- src/inference/utils.py
+++ src/inference/utils.py
@@ -3,9 +3,13 @@
     utils
     ~~~~~
 
-    Model inference wrapper utilities, such as default prompts and functions.
+    Model inference utilities.
 
 """
+
+import os
+
+# =======================================================
 
 def contains(text: str, words):
     """ Checks if a string contains any of the given words in an iterable. """
@@ -18,3 +22,47 @@ def is_conversation(prompt: list|str):
             isinstance(prompt[0], list) and isinstance(prompt[0][0], dict)
         )
     )
+
+# =======================================================
+
+def seed_all(seed=None):
+    import time
+    import transformers
+
+    transformers.set_seed(seed or time.time_ns())
+
+def sync_vram():
+    """ Synchronizes GPU memory to free resources. """
+
+    import gc
+    import torch
+
+    if not torch.cuda.is_available(): return
+
+    while gc.collect() > 0:
+        torch.cuda.empty_cache()
+        torch.cuda.synchronize()
+        torch.cuda.reset_peak_memory_stats()
+
+# =======================================================
+
+OPENAI_CREDENTIALS = {
+    'org_id' : os.environ.get('OPENAI_ORG_ID'),
+    'api_key': os.environ.get('OPENAI_API_KEY')
+}
+
+AZURE_OPENAI_CREDENTIALS = {
+    'endpoint'   : os.environ.get('AZURE_OPENAI_ENDPOINT'),
+    'api_key'    : os.environ.get('AZURE_OPENAI_API_KEY'),
+    'deployment' : os.environ.get('AZURE_OPENAI_DEPLOYMENT'),
+    'api_version': os.environ.get('AZURE_OPENAI_API_VERSION')
+}
+
+def get_inference_params(model_name):
+    if "openai" in model_name.lower():
+        return AZURE_OPENAI_CREDENTIALS if "azure" in model_name.lower() else OPENAI_CREDENTIALS
+    else:
+        std_kwargs = dict(torch_dtype='bfloat16', device_map='auto')
+        # Follows from: https://github.com/huggingface/transformers/issues/32390
+        if "gemma" in model_name.lower(): std_kwargs['attn_implementation'] = 'flash_attention_2'
+        return std_kwargs
