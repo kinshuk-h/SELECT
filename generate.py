8c29d5135486cc3a0e7ef67f91542e26ff091e77
Kinshuk Vasisht <kinshukv@iisc.ac.in>
2025-02-05 20:00:06 +0530

    Finalize refactor in `generate.py`

--- generate.py
+++ generate.py
@@ -12,26 +12,29 @@ def main():
     parser = common.make_parser('generate', MODELS, get_techniques(), TYPES)
     args   = parser.parse_args()
 
+    models      = [ MODELS.get(model, model) for model in (args.models or MODELS) ]
+    excl_models = set(MODELS.get(model, model) for model in args.exclude_models)
+    resolved_models = [ model for model in models if model not in excl_models ]
+
     kwargs = dict(
         seed         = args.seed,
         num_seeds    = args.num_seeds,
         sample       = args.sample,
         batch_size   = args.batch_size,
         save_steps   = args.save_every,
-        models       = common.gather(MODELS, (set(args.models) - set(args.exclude_models or []))),
-        techniques   = common.gather(get_techniques(), args.techniques)
+        models       = list(dict.fromkeys(resolved_models)),
+        approaches   = common.gather(get_techniques(), args.approaches)
     )
 
     dataset_dtype = 'compose' if args.compose else 'atom'
     dtype_suffix  = '' if dataset_dtype == 'atom' else f'.{dataset_dtype}'
 
     with common.LogTime(f"Loading primary dataset state ({dataset_dtype})", verbose=False):
-        kwargs['dataset_state'] = dataset.DatasetState.from_name(args.dataset, dataset_dtype)
+        kwargs['dataset_state'] = dataset.DatasetState('select', args.data_dir, compose=args.compose)
 
     if args.compose:
         with common.LogTime(f"Loading dataset state (atom)", verbose=False):
-            kwargs['atomic_ds_state'] = dataset.DatasetState.from_name(args.dataset, 'atom')
-
+            kwargs['atomic_ds_state'] = dataset.DatasetState('select', args.data_dir, compose=False)
 
     results_dir = args.root_dir / f"expr.abstain.select{dtype_suffix}"
     evaluator = task.Evaluator(root=results_dir, **kwargs)
