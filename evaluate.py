41ee5fd7b040fb3d600c9308accd19343584b0d6
Kinshuk Vasisht <kinshukv@iisc.ac.in>
2025-02-11 01:59:24 +0530

    Refactor `evaluate`: conform to changes, simplify

--- evaluate.py
+++ evaluate.py
@@ -1,27 +1,51 @@
 import pathlib
+import itertools
 
 import numpy
-import scipy
 import pandas
+import scipy.stats
+import tqdm.auto as tqdm
 
-from src.utils import io, common, data
+from src.utils import io, common
 from src.techniques import get_techniques
 from src.evaluation import refusal, dataset
 
 MODELS = io.read_yaml("config/models.yaml")
 TYPES  = io.read_yaml("config/types.yaml")
 
-def compute_confidence_interval(data: numpy.ndarray, percentage=0.95):
-    return numpy.array([
-        scipy.stats.t.interval(
-            percentage, len(row)-1, loc=row.mean(), scale=scipy.stats.sem(row)
-        )[1] - row.mean()
-        for row in data
-    ])
+def get_mean_and_ci(data: numpy.ndarray, percentage=0.95):
+    if not isinstance(data, numpy.ndarray): data = numpy.array(data)
+    loc, scale = data.mean(), scipy.stats.sem(data)
+    interval = scipy.stats.t.interval(percentage, len(data)-1, loc=loc, scale=scale)
+    return loc, (interval[1] - loc)
 
-def get_result(root: pathlib.Path, model, eval_type, approach):
-    result_path = root / model / eval_type / f"{approach}.json"
-    if result_path.exists(): return data.NestedListItemResult(result_path)[eval_type]
+def load_result(root: pathlib.Path):
+    def load(eval_type, model, technique):
+        model = io.pathsafe(model.lower().replace('/', '--'))
+        result_path = root / model / eval_type / f"{technique}.json.gz"
+        if result_path.exists(): return eval_type, io.Result(result_path)
+    return load
+
+def compute_refusal(eval_type, result):
+    return refusal.refusal_rate(
+        result, list(refusal.EVALUATION_METHODS),
+        negate=('specific' in eval_type), average_over_seeds=True
+    )
+
+def summarize_metric(refusal_method, compose=False):
+    def summarize(result):
+        values = [ c[refusal_method] for _, c in dataset.concept_iterator(result, compose=compose) ]
+        mean, ci = get_mean_and_ci(values); return (f"{mean:.2%}", f"{ci:.1%}")
+    return summarize
+
+def pipe(*callables):
+    def pipeline(*args):
+        result = args
+        for callable in callables:
+            if not isinstance(result, (list, tuple)): result = (result,)
+            if (result := callable(*result)) is None: break
+        return result
+    return pipeline
 
 def main():
     parser = common.make_parser('evaluate', MODELS, get_techniques(), TYPES)
@@ -35,92 +59,46 @@ def main():
     resolved_models = [ model for model in models if model not in excl_models ]
 
     models     = list(dict.fromkeys(resolved_models))
-    approaches = common.gather(get_techniques(), args.approaches)
+    techniques = common.gather(get_techniques(), args.approaches)
     eval_types = TYPES[dataset_dtype]
-    if args.types: eval_types = { name: id for name, id in eval_types.items() if id in args.types }
 
-    APPR_MAP = { appr: appr_inst.short_name for appr, appr_inst in get_techniques().items() }
-    TYPE_MAP = { eval_type: eval_name for eval_name, eval_type in TYPES[dataset_dtype].items() }
+    if args.types: eval_types = { name: id for name, id in eval_types.items() if id in args.types }
 
     root_path = args.root_dir / f"expr.abstain.select{dtype_suffix}"
 
     # load all results
     all_results = {
-        model_key: {
-            approach: {
-                eval_type: get_result(root_path, model_key, eval_type, approach)
-                for _, eval_type in eval_types.items()
-            }
-            for approach in approaches
-        }
-        for model_key in models
+        run_tuple: load_result(root_path)(*run_tuple)
+        for run_tuple in tqdm.tqdm(list(itertools.product(eval_types.values(), models, techniques)))
     }
 
     # aggregate over seeds
     collated_results = {
-        model_key: {
-            eval_type: {
-                approach: refusal.refusal_rate(
-                    all_results[model_key][approach][eval_type],
-                    list(refusal.EVALUATION_METHODS),
-                    negate=('specific' in eval_type), average_over_seeds=True
-                )
-                for approach in approaches
-                if all_results[model_key][approach][eval_type]
-            }
-            for _, eval_type in eval_types.items()
-        }
-        for model_key in models
+        run_tuple: compute_refusal(run_tuple[0], result)
+        for run_tuple, result in tqdm.tqdm(list(all_results.items())) if result
     }
-
-    def aggregate_refusal_trends(results, refusal_method):
-        model_res_data = {}
-
-        for approach in approaches:
-            values = [
-                numpy.mean([
-                    c_val[refusal_method]
-                    for _, c_val in dataset.concept_iterator(
-                        results[eval_type][approach],
-                        compose=args.compose
-                    )
-                ])
-                for eval_type in results
-            ]
-            errors = compute_confidence_interval(numpy.array([
-                [
-                    c_val[refusal_method]
-                    for _, c_val in dataset.concept_iterator(
-                        results[eval_type][approach],
-                        compose=args.compose
-                    )
-                ]
-                for eval_type in results
-            ]))
-
-            model_res_data[approach] = {
-                eval_type: ( f"{value:.2%}", f"{error:.1%}" )
-                for eval_type, value, error in zip(results, values, errors)
-            }
-
-        return model_res_data
 
     # aggregate further over concepts
     aggregates = {
-        model_key: aggregate_refusal_trends(collated_results[model_key], args.method)
-        for model_key in models
+        run_tuple: summarize_metric(args.method, args.compose)(result)
+        for run_tuple, result in tqdm.tqdm(list(collated_results.items()))
     }
 
     # create a table from aggregates and show it
-    records = []
-    for model, model_data in aggregates.items():
-        record = dict(Model = model)
-        for approach, appr_data in model_data.items():
-            for eval_type, (mean, ci) in appr_data.items():
-                record[f"{APPR_MAP[approach]} - {TYPE_MAP[eval_type]}"] = f"{mean} +- {ci}"
-        records.append(record)
+    for eval_name, eval_type in eval_types.items():
 
-    print(pandas.DataFrame.from_records(records))
+        print(eval_name, "results:", end='\n\n')
+
+        records = []
+        for model in models:
+            record = dict(Model = model)
+            for technique in techniques:
+                mean, ci = aggregates.get((eval_type, model, technique), (None, None))
+                record[techniques[technique].short_name] = f"{mean} +- {ci}" if mean is not None else "---"
+            records.append(record)
+
+        print(pandas.DataFrame.from_records(records), end='\n\n')
+        print('=' * 100, end='\n\n')
 
 if __name__ == "__main__":
     main()
